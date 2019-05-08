a3401399d261ca393d3bb680751b2f4c40613bdb
Ray <raymondgiang@gmail.com>
2019-05-08 12:52:06 -0400

    added player and movement

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,17 +1,16 @@
+"use strict";
+
 function ChallengeCanvas() {
 	this.canvas = document.createElement('canvas');
 	this.canvas.id = 'challenge-canvas';
-	this.canvas.width = window.innerWidth / 2;
-	this.canvas.height = window.innerHeight / 2;
 
 	this.context = this.canvas.getContext('2d');
-	this.gridHeight = this.canvas.height / 15;
-	this.gridWidth = this.canvas.width / 20;
 
-	this.getCanvas = function() {
-		return this.canvas;
-	}
+	// grid will be widthInTiles x heightInTiles
+	this.widthInTiles = 30;
+	this.heightInTiles = 15;
 
+    // method used to draw a 30x15 grid on the canvas of tile size this.canvas.width
 	this.drawGrid = function() {
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
@@ -23,22 +22,59 @@ function ChallengeCanvas() {
 
 		while(currentX < this.canvas.width) {
 			while(currentY < this.canvas.height) {
-				this.context.rect(currentX, currentY, this.gridWidth, this.gridHeight);
-				currentY += this.gridHeight;
+				this.context.rect(currentX, currentY, this.gridSize, this.gridSize);
+				currentY += this.gridSize;
 			}
 			currentY = 0;
-			currentX += this.gridWidth;
+			currentX += this.gridSize;
 		}
 
 		this.context.stroke();
-	}
+	};
 
-	this.canvasResize = function() {
-		this.canvas.width = window.innerWidth / 2;
-		this.canvas.height = window.innerHeight / 2;
-		this.gridHeight = this.canvas.height / 15;
-		this.gridWidth = this.canvas.width / 20;
+	// method used to resize the canvas and redraw the grid accordingly
+	this.canvasRefresh = function() {
+		if(window.innerWidth < 1200) {
+			this.canvas.width = window.innerWidth - 50;
+		}
+		else {
+			this.canvas.width = 1200;
+		}
+
+		this.gridSize = this.canvas.width / this.widthInTiles;
+		if(this.player) {
+			this.player.gridSize = this.gridSize;
+		}
+
+		this.canvas.height = this.gridSize * this.heightInTiles;
 
 		this.drawGrid();
+
+		if(this.player){
+			this.renderPlayer();
+		}
+	};
+
+	this.initializePlayer = function() {
+		this.player = new Player(this.widthInTiles, this.heightInTiles, 1, 1);
+		this.renderPlayer();
+	};
+
+	this.renderPlayer = function() {
+		this.context.fillStyle = "fuchsia";
+		this.context.fillRect(
+			this.player.xPosition * this.gridSize, 
+			this.player.yPosition * this.gridSize,
+			this.gridSize,
+			this.gridSize
+		);
 	}
+
+	this.movePlayer = function(direction) {
+		this.player.move(direction);
+		this.canvasRefresh();
+	}
+
+	this.canvasRefresh();
+	this.initializePlayer();
 }
