87ddfdf3dd718c50ae8408b02aa6df9391a165a8
Kento Kanazawa <kentokanazawa@yahoo.com>
2019-05-13 14:13:04 -0400

    reverted changes made to login

--- index.php
+++ index.php
@@ -10,24 +10,4 @@ if (isset($_SESSION['userid']) && !empty($_SESSION['userid'])) {
   // redirect to profile
   header('Location: profile.php');
 } else {
-  return $googleLoginBtn = '<a href="googleauth.php"><img src="images/btn_google_signin_light_normal_web.png" alt="login with google button"/></a>';
-}
-?>
-<!DOCTYPE html>
-<html lang='en'>
-
-<head>
-  <title>Enter the Codegeon</title>
-  <meta charset="utf-8">
-</head>
-
-<body>
-  <form action='' method='POST'>
-    <input type='text'>
-    <input type='password'>
-    <button type="submit">Login</button>
-    <?= $googleLoginBtn ?>
-  </form>
-</body>
-
-</html>
+  echo $googleLoginBtn = '<a href="googleauth.php"><img src="images/btn_google_signin_light_normal_web.png" alt="login with google button"/></a>';
