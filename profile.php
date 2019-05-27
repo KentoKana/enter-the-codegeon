86cbf443c68ddfb78ba11abd6cd70f7d7f61bf53
Kento Kanazawa <kentokanazawa@yahoo.com>
2019-05-26 22:49:57 -0400

    added markup for profile view

--- profile.php
+++ profile.php
@@ -16,10 +16,32 @@ require_once './controllers/user-controller.php';
 <body>
   <div>
     <h1>Welcome, <?= (string)$user->firstName ?></h1>
+    <!-- Test Image File -->
     <img src="./images/user.png" alt="User Profile" width="100">
     <form action="" method="POST">
       <button type="submit" name="logout">Log out</button>
     </form>
+    <div class="profile__playGame">
+      <h2><a href="./stage-picker.php">Play Enter the Codegeon!</a></h2>
+
+    </div>
+    <div class="profile__achievements">
+      <h2>Achievements</h2>
+      <?php
+        $testArr = [
+          'Achievement1',
+          'Achievement2',
+          'Achievement3',
+          'Achievement4',
+          'Achievement5'
+        ];
+
+        foreach($testArr as $item) {
+          echo "<div>$item</div>";
+        }
+      ?>
+
+    </div>
   </div>
 </body>
 
