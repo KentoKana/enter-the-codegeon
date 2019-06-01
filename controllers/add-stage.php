5a74aa2e6f054a414fb63b4e071b557529d69901
Ray <n01304390@humbermail.ca>
2019-06-01 17:34:09 -0400

    split stage logic in controller

---
+++ controllers/add-stage.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+require_once '../vendor/autoload.php';
+require_once '../Models/Stage.php';
+
+$input = file_get_contents("php://input");
+
+$client = new MongoDB\Client(env('MONGO_URI'));
+$collection = $client->codegen->mazeStages;
+$stage = new Stage($collection);
+
+$inputData = json_decode($input);
+
+$stage->addStage($inputData);
+
+echo 'Stage Added';
