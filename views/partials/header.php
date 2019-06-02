c410be01eaca10197edae252c3e1a6f638b0e342
Mark Martin <n01321102@humbermail.ca>
2019-06-02 19:53:10 -0400

    changed some fonts and fixed the icon font issue

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Enter the Codegeon</title>
     <meta name="viewport" content="width=device-width">
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:700|Ubuntu&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="public/css/style.css">
     <link rel="stylesheet" type="text/css" href="public/css/more.css">
     <link rel="stylesheet" href="public/css/code.css">
