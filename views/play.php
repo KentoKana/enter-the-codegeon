44b48f86b2f7b311ead2b8477c0bef1ed4c7c60e
adenoidhynkel <n01321102@humbermail.ca>
2019-06-02 17:37:02 -0400

    changed button icons, and fixed div sizing

--- views/play.php
+++ views/play.php
@@ -10,9 +10,11 @@ require_once 'partials/navigation.php';
 <input type="hidden" id="startCoord" value="<?=$_SESSION['startCoord']?>">
 <input type="hidden" id="goalCoord" value="<?=$_SESSION['goalCoord']?>">
 <input type="hidden" id="obsCoords" value="<?=$_SESSION['obsCoords']?>">
-
-<div id="playing-area"></div>
-<div id="coding-area">
+  <header class="block">
+   <h1 id="score">Score:</h1>
+  </header>
+  <div id="playing-area"></div>
+ <div id="coding-area">
   <div class="btn__block">
     <button id="turn-left" value="0" class="movement-buttons">Turn Left</button>
     <button id="move-forward" value="1" class="movement-buttons">Move Forward</button>
@@ -23,7 +25,12 @@ require_once 'partials/navigation.php';
     <button id="start-button" class="action-buttons">Run</button>
     <button id="quit-button" class="action-buttons">Quit Game</button>
   </div>
-  <div id="move-list" class="block">Move List: </div>
-</div>
+  <div id="play">
+   <header>
+    <h2 class="charcoal">Actions</h2>
+   </header>
+   <div id="move-list"></div>
+  </div>
+ </div>
 <?php
 require_once 'partials/footer.php';
